3005682c923c39f998d30f56771593759a9090bf
Kauã Menezes <kauavictor.f.menezes@hotmail.com>
2026-05-07 16:37:18 -0300

    feat: implementação da classe Order (responsavel pelo complemento final das informações do pedido + verificações do status de pagamento + lista de itens)

---
+++ src/model/entities/Order.java
@@ -0,0 +1,56 @@
+package model.entities;
+
+import model.enums.OrderStatus;
+
+import java.time.LocalTime;
+import java.util.List;
+
+public class Order {
+
+    private Integer tableNumber;
+    private OrderStatus status;
+    private LocalTime time;
+    private List<OrderItem> items;
+
+    public Order(Integer tableNumber) {
+        this.tableNumber = tableNumber;
+        this.status = OrderStatus.PENDING;
+        this.time = LocalTime.now();
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public void setTableNumber(Integer tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public Integer getTableNumber() {
+        return tableNumber;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void addItem(OrderItem item) {
+        items.add(item);
+    }
+
+    public boolean isPending(){
+        return status == OrderStatus.PENDING;
+    }
+
+    public boolean isPaid(){
+        return status == OrderStatus.PAID;
+    }
+}
